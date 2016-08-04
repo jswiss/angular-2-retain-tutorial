a31193c002843c5b42bb557deb0f9582e7a5095d
Joshua Swiss <swissjoshua@gmail.com>
2016-08-04 13:51:06 +0300

    lesson 2 down, wrote first directive and added containers folder and main container

---
+++ src/app/containers/main.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-container',
+  template: `
+    <div>
+      <main class="main">
+        Content goes here!
+      </main>
+    </div>
+  `
+})
+export class Main {}
